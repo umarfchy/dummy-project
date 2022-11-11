67644204e4f5724cda922bb85f1a3df0302076a5
Umar Chowdhury <umarfchy@gmail.com>
2022-11-11 06:49:13 +0600

    add docker file

---
+++ README.md
@@ -0,0 +1,3 @@
+# demo-project
+> node v14.21.1,
+> npm 8.19.3
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h2>Welcome!</h2>
+          <h2>This application is up and running.(main)</h2>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
